a4088c38d65775d4357a01bf1d88c2c0b3f64377
Adrian <work.adrianfigueroa@gmail.com>
2019-08-29 06:05:58 +0000

    update

--- TeslaStock.R
+++ TeslaStock.R
@@ -6,77 +6,124 @@ tsla_stock <- read.csv(
   sep = ",",
   stringsAsFactors = FALSE
 )
-View(tsla_stock)
 
 #Setup
-tsla_stock.setup <- subset(
+tsla_stock <- subset(
   x = tsla_stock,
   select = c(Date, Open, High, Low, Close, Adj.Close, Volume)
 )
 
 #Variacion durante horas activas
-vha.tsla_stock <- tsla_stock.setup
+##### 
+vha.tsla_stock <- tsla_stock
 
 #Open - High
 vha.tsla_stock.oh <- (vha.tsla_stock$High - vha.tsla_stock$Open)
+df.vha.tsla_stock.oh <- data.frame(vha.tsla_stock.oh)
+names(df.vha.tsla_stock.oh) <- c("Open-High")
 #Open - Low
 vha.tsla_stock.ol <- (vha.tsla_stock$Low - vha.tsla_stock$Open)
+df.vha.tsla_stock.ol <- data.frame(vha.tsla_stock.ol)
+names(df.vha.tsla_stock.ol) <- c("Open-Low")
 #Open - Close
 vha.tsla_stock.oc <- (vha.tsla_stock$Close - vha.tsla_stock$Open)
-
-#Set c
-c <- 0
-c5 <- 0
-c10 <- 0
-c15 <- 0
-c20 <- 0
-c25 <- 0
+df.vha.tsla_stock.oc <- data.frame(vha.tsla_stock.oc)
+names(df.vha.tsla_stock.oc) <- c("Open-Close")
 
 #Cant. Open - High 0-5
-vha.tsla_stock.oh.val0_5 <- 
-  sum(ifelse(vha.tsla_stock.oh>0,
-       ifelse(vha.tsla_stock.oh<=5,
-              c <- c+1,
-              c <- 0),
-       c <- c))
-
+df.vha.tsla_stock.oh.val0_5 <- subset(
+  x = df.vha.tsla_stock.oh,
+  subset = `Open-High` > 0 & `Open-High` <= 5,
+  select = c("Open-High")
+)
 #Cant. Open - High 5-10
-vha.tsla_stock.oh.val5_10 <- 
-  sum(ifelse(vha.tsla_stock.oh>5,
-             ifelse(vha.tsla_stock.oh<=10,
-                    c5 <- c5+1,
-                    c5 <- 0),
-             c5 <- c5))
-
+df.vha.tsla_stock.oh.val5_10 <- subset(
+  x = df.vha.tsla_stock.oh,
+  subset = `Open-High` > 5 & `Open-High` <= 10,
+  select = c("Open-High")
+)
 #Cant. Open - High 10-15
-vha.tsla_stock.oh.val10_15 <- 
-  sum(ifelse(vha.tsla_stock.oh>10,
-             ifelse(vha.tsla_stock.oh<=15,
-                    c10 <- c10+1,
-                    c10 <- 0),
-             c10 <- c10))
-
+df.vha.tsla_stock.oh.val10_15 <- subset(
+  x = df.vha.tsla_stock.oh,
+  subset = `Open-High` > 10 & `Open-High` <= 15,
+  select = c("Open-High")
+)
 #Cant. Open - High 15-20
-vha.tsla_stock.oh.val15_20 <- 
-  sum(ifelse(vha.tsla_stock.oh>15,
-             ifelse(vha.tsla_stock.oh<=20,
-                    c15 <- c15+1,
-                    c15 <- 0),
-             c15 <- c15))
-
+df.vha.tsla_stock.oh.val15_20 <- subset(
+  x = df.vha.tsla_stock.oh,
+  subset = `Open-High` > 15 & `Open-High` <= 20,
+  select = c("Open-High")
+)
 #Cant. Open - High 20-25
-vha.tsla_stock.oh.val20_25 <- 
-  sum(ifelse(vha.tsla_stock.oh>20,
-             ifelse(vha.tsla_stock.oh<=25,
-                    c20 <- c20+1,
-                    c20 <- 0),
-             c20 <- c20))
-
+df.vha.tsla_stock.oh.val20_25 <- subset(
+  x = df.vha.tsla_stock.oh,
+  subset = `Open-High` > 20 & `Open-High` <= 25,
+  select = c("Open-High")
+)
 #Cant. Open - High 25-37
-vha.tsla_stock.oh.val25_37 <- 
-  sum(ifelse(vha.tsla_stock.oh>25,
-             ifelse(vha.tsla_stock.oh<=37,
-                    c25 <- c25+1,
-                    c25 <- 0),
-             c25 <- c25))
+df.vha.tsla_stock.oh.val25_37 <- subset(
+  x = df.vha.tsla_stock.oh,
+  subset = `Open-High` > 25 & `Open-High` <= 37,
+  select = c("Open-High")
+)
+
+vha.tsla_stock.cant <- c(
+  nrow(df.vha.tsla_stock.oh.val0_5),
+  nrow(df.vha.tsla_stock.oh.val5_10),
+  nrow(df.vha.tsla_stock.oh.val10_15),
+  nrow(df.vha.tsla_stock.oh.val15_20),
+  nrow(df.vha.tsla_stock.oh.val20_25),
+  nrow(df.vha.tsla_stock.oh.val25_37)
+)
+
+vha.tsla_stock.avg <- c(
+  mean(df.vha.tsla_stock.oh.val0_5$`Open-High`),
+  mean(df.vha.tsla_stock.oh.val5_10$`Open-High`),
+  mean(df.vha.tsla_stock.oh.val10_15$`Open-High`),
+  mean(df.vha.tsla_stock.oh.val15_20$`Open-High`),
+  mean(df.vha.tsla_stock.oh.val20_25$`Open-High`),
+  mean(df.vha.tsla_stock.oh.val25_37$`Open-High`)
+)
+
+vha.tsla_stock.hdate <- c(
+  
+)
+
+vha.tsla_stock.hprice <- c(
+  max(df.vha.tsla_stock.oh.val0_5$`Open-High`),
+  max(df.vha.tsla_stock.oh.val5_10$`Open-High`),
+  max(df.vha.tsla_stock.oh.val10_15$`Open-High`),
+  max(df.vha.tsla_stock.oh.val15_20$`Open-High`),
+  max(df.vha.tsla_stock.oh.val20_25$`Open-High`),
+  max(df.vha.tsla_stock.oh.val25_37$`Open-High`)
+)
+
+vha.tsla_stock.ldate <- c(
+  
+)
+
+vha.tsla_stock.lprice <- c(
+  
+)
+
+
+vha.tsla_stock.stats <- data.frame(
+  vha.tsla_stock.t,
+  vha.tsla_stock.avg,
+  vha.tsla_stock.hdate,
+  vha.tsla_stock.hprice,
+  vha.tsla_stock.ldate,
+  vha.tsla_stock.lprice
+)
+#####
+
+#Variacion durante horas inactivas
+#####
+
+
+#####
+
+
+
+
 
